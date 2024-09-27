4bdbf7e0f8721f7c01c3786ac1179cee7e2c6d5f
craig-grant630 <pc8hxmen@students.codeinstitute.net>
2024-09-27 20:43:22 +0000

    Add data types for buttons and display an alert when clicked from js

---
+++ assests/js/script.js
@@ -0,0 +1,17 @@
+/*Wait for the DOM to finish loading before running the game*/
+
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByTagName("button"); /*buttons outputs an array of all buttons */
+    /*retrieve the data type of each button to distigush what button does what*/
+    /*Get users choice and rules button*/
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
+            if (this.getAttribute("data-type") === "rules-btn") {
+                alert("You clicked rules");
+            } else {
+                let userChoice = this.getAttribute("data-type");
+                alert(`You clicked ${userChoice}.`);
+            }
+        })
+    }
+})
